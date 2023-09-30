c007cda7c7965e1a585b24acbc072a7a351f5637
Alisson Lima <alissonlima0860@gmail.com>
2023-09-30 16:34:48 -0300

    base1

---
+++ src/main/java/br/unitins/topicos1/model/User.java
@@ -0,0 +1,44 @@
+package br.unitins.topicos1.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+    private String username;
+    private String email;
+    private String password;
+    private Person person;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+}
